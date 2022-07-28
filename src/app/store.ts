83560218ed9ca79c6a3fb675ba76d3c5f7ff2921
Surya <123004250@sastra.ac.in>
2022-07-28 21:14:45 +0530

    feat: added users and posts to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  authReducer from "../features/authentication/authSlice";
-
+import usersReducer from '../features/users/usersSlice';
+import postsReducer from '../features/posts/postsSlice';
 export const store = configureStore({
   reducer: {
     auth:authReducer,
+    users:usersReducer,
+    posts:postsReducer,
   },
 });
 
