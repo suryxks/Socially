99beee5a05b1b94c135606ad5676b0e496134057
Surya <123004250@sastra.ac.in>
2022-07-28 21:12:41 +0530

    feat: added slices for auth, posts and users

--- src/features/authentication/authSlice.ts
+++ src/features/authentication/authSlice.ts
@@ -1,45 +1,17 @@
-import { RootState } from './../../app/store';
-import { authFormData } from "../../pages/LoginPage";
+import { RootState } from "./../../app/store";
+import { authFormData } from "../../app/types";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import axios from "axios";
-
-interface authState {
-  //   name: string;
-  username: string;
-  isAuthenticated: boolean;
-  error: string;
-  encodedToken: string;
-}
-interface user {
-  bio: string;
-  bookmarks: [];
-  createdAt: string;
-  firstName: string;
-  followers: [];
-  following: [];
-  id: string;
-  lastName: string;
-  password: string;
-  updatedAt: string;
-  username: string;
-  _id: string;
-}
-interface authApiReturnType {
-  foundUser: user;
-  encodedToken: string;
-}
+import { authState, authApiReturnType } from "../../app/types";
 
 const initialState: authState = {
+  userData: null,
   username: "",
   isAuthenticated: false,
   error: "",
   encodedToken: "",
 };
-// interface MyData{
-// user:user,
-// encodedToken:string
-// }
 export const login = createAsyncThunk(
   "auth/login",
   async (userDetails: authFormData, thunkAPI) => {
@@ -47,7 +19,6 @@ export const login = createAsyncThunk(
       const respnse = await axios.post("/api/auth/login", userDetails);
       return respnse.data as unknown as authApiReturnType;
     } catch (error) {
-      console.log(error);
       return thunkAPI.rejectWithValue(error);
     }
   }
@@ -55,16 +26,22 @@ export const login = createAsyncThunk(
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    logout: () => {
+      return initialState;
+    },
+  },
   extraReducers(builder) {
     builder.addCase(login.fulfilled, (state, action) => {
       const { foundUser, encodedToken } = action.payload;
+      state.userData=foundUser;
       state.username = foundUser.username;
       state.encodedToken = encodedToken;
       state.isAuthenticated = true;
     });
   },
 });
-export const authStateSelector=(state:RootState)=>state.auth;
+export const { logout } = authSlice.actions;
+export const authStateSelector = (state: RootState) => state.auth;
 
 export default authSlice.reducer;
---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,35 @@
+import { RootState } from "app/store";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { post } from "app/types";
+import axios from "axios";
+
+const initialState: postsType = {
+  posts: [],
+};
+type postsType = {
+  posts: post[];
+};
+export const getAllPosts = createAsyncThunk(
+  "posts/getallPosts",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/api/posts");
+      console.log(response.data);
+      return response.data as postsType;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllPosts.fulfilled, (state, action) => {
+      state.posts = action.payload.posts;
+    });
+  },
+});
+
+export default postsSlice.reducer;
