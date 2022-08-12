9c1c8a8072b8e5aa1b16e8af7f34cec82b0cea96
Surya <123004250@sastra.ac.in>
2022-08-12 06:47:51 +0530

    added menu component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-
+import { Avatar } from "@mui/material";
+import { MdAdd } from 'react-icons/md'
 import { useAppSelector, useAppDispatch } from "app/hooks";
+import { IconContainer } from "./AppNavbar";
 import { authStateSelector, logout } from "features/authentication/authSlice";
+import { setPostModalContent,openPostModal,closePostModal } from "features/modals/modalSlice";
 import { ButtonCta } from "./ButtonCta";
 import { StyledLink } from "./StyledLink";
-
+import { Menu } from "./Menu";
 export const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
+    const toggleMenu = () => setIsMenuOpen(state => !state);
     const auth = useAppSelector(authStateSelector)
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
@@ -15,15 +20,34 @@ export const Header = () => {
     return (
         <HeaderWrapper>
             <StyledLink to='/home'>Socially</StyledLink>
-            <ButtonCta onClick={() => {
-                if (isAuthenticated) {
-                    dispatch(logout())
-                } else {
-                    navigate('/')
+            <HeaderActions>
+                {isAuthenticated ? <>
+                    <ButtonCta onClick={() => {
+                        dispatch(openPostModal())
+                    }}>
+                        <IconContainer>
+                            <MdAdd />
+                        </IconContainer>New Post</ButtonCta>
+                    <Avatar src={auth.userData?.avatarURL} alt={auth.username} sx={{ border: '2px solid grey', cursor: 'pointer' }} onClick={toggleMenu} />
+                </>
+                    : (
+                        <ButtonCta onClick={() => {
+                            if (isAuthenticated) {
+                                dispatch(logout())
+                            } else {
+                                navigate('/')
+                            }
+                        }}>{isAuthenticated ? 'logout' : 'Sign in'}</ButtonCta>)
                 }
-            }}>{isAuthenticated ? 'logout' : 'Sign in'}</ButtonCta>
+                <Menu isActive={isMenuOpen} setIsActive={setIsMenuOpen} />
+            </HeaderActions>
         </HeaderWrapper>)
 }
+const HeaderActions = styled.div`
+    display: flex;
+    align-items: center;
+    gap:1rem;
+`
 const HeaderWrapper = styled.nav`
     position: sticky;
     top:0;
