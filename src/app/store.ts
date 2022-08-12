7547d24d7ab94fcf4e6a33bb947e3a43bb0d02d1
Surya <123004250@sastra.ac.in>
2022-08-12 06:48:54 +0530

    feat: implemented likes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  authReducer from "../features/authentication/authSlice";
-import usersReducer from '../features/users/usersSlice';
-import postsReducer from '../features/posts/postsSlice';
+import authReducer from "../features/authentication/authSlice";
+import usersReducer from "../features/users/usersSlice";
+import postsReducer from "../features/posts/postsSlice";
+import modalReducer from "features/modals/modalSlice";
+import bookmarksReducer from "features/bookmarks/bookmarksSlice";
 export const store = configureStore({
   reducer: {
-    auth:authReducer,
-    users:usersReducer,
-    posts:postsReducer,
+    auth: authReducer,
+    users: usersReducer,
+    posts: postsReducer,
+    modal: modalReducer,
+    bookmarks: bookmarksReducer,
   },
 });
 
-export type RootState=ReturnType<typeof store.getState>
-export type AppDispatch=typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -4,9 +4,14 @@ import axios from "axios";
 
 const initialState: postsType = {
   posts: [],
+ 
 };
 type postsType = {
   posts: post[];
+};
+type postRequest = {
+  token: string;
+  postContent: string;
 };
 export const getAllPosts = createAsyncThunk(
   "posts/getallPosts",
@@ -19,14 +24,69 @@ export const getAllPosts = createAsyncThunk(
     }
   }
 );
+export const createPost = createAsyncThunk(
+  "posts/createPost",
+  async ({ token, postContent }: postRequest, thunkAPI) => {
+    try {
+      const response = await axios.post(
+        "/api/posts",
+        { postData: postContent },
+        { headers: { authorization: token } }
+      );
+      return response.data as postsType;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const likePost = createAsyncThunk(
+  "posts/like",
+  async ({ token, postId }: { token: string; postId: string }, thunkAPI) => {
+    try {
+      const { data } = await axios.post(
+        `/api/posts/like/${postId}`,
+        {},
+        { headers: { authorization: token } }
+      );
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const dislikePost = createAsyncThunk(
+  "posts/dislike",
+  async ({ token, postId }: { token: string; postId: string }, thunkAPI) => {
+    try {
+      const { data } = await axios.post(
+        `/api/posts/dislike/${postId}`,
+        {},
+        { headers: { authorization: token } }
+      );
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 const postsSlice = createSlice({
   name: "posts",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getAllPosts.fulfilled, (state, action) => {
-      state.posts = action.payload.posts;
-    });
+    builder
+      .addCase(getAllPosts.fulfilled, (state, action) => {
+        state.posts = action.payload.posts;
+      })
+      .addCase(createPost.fulfilled, (state, action) => {
+        state.posts = action.payload.posts;
+      })
+      .addCase(likePost.fulfilled, (state, action) => {
+        state.posts = action.payload.posts;
+      })
+      .addCase(dislikePost.fulfilled, (state, action) => {
+        state.posts = action.payload.posts;
+      });
   },
 });
 
